bf894363fb9510164836efab79bf1e86849c245f
juansegarod <juansega96@gmail.com>
2024-04-22 10:35:20 -0500

    se agrego las actividades del dia

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,33 @@
 package com.mycompany.libropoo;
 
-public class Libro {
+public class Libro
+
+{
+    String titulo;
+    String autor;
+    int anoPublicacion;
+    boolean leido = false;
     
+  public Libro mostrarInformacion(String titulo, String autor,int anoPublicacion )
+          
+  {
+    
+      this.titulo = titulo;
+      this.autor = autor;
+      this.leido = false;
+     return null;
+      
+  }
+  
+  public void marcarLeido() {
+      this.leido=true;
+      boolean mostrarInformacion = false;
+      System.out.println(mostrarInformacion);
+      
+      
+      
+  }
+  
+  
+  
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,26 @@
 package com.mycompany.libropoo;
 
-public class Principal {
+import java.util.Scanner;
+
+public class Principal
+{
+    static Scanner sc = new Scanner (System.in);
+    public static void main(String[] args)
+    {
+    
+        System.out.println("Ingrese titulo del Libro ");
+        String titulo=sc.nextLine();
+        System.out.println("Ingrese autor del libro ");
+        String autor=sc.nextLine();
+        System.out.println("Ingrese año de publicacion ");
+        String anodePublicacion=sc.nextLine();
+        
+       Libro libro1 = new Libro(""); 
+   
+       libro1.marcarLeido();
+        
+    
+    
+    }
     
 }
